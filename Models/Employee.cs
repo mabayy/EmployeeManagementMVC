d0d70dfc5eaff05a7dd42d61e249bc36d2f149f8
Mahesha Bayu <bayupaksi2007@gmail.com>
2024-10-10 15:36:10 +0700

    CRUD Employee

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public int PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Designation { get; set; }
+    }
+}
